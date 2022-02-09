113f624b0743f2801cfcd18f639af158517a0b46
varlamova <varlamvanadia@mail.ru>
2022-02-09 19:26:06 +0300

    add tests
    add test 3==3
    
    add test 1!=3
    
    SimpleTest1
    
    SimpleTest2
    
    SimpleTest3

--- src/test/java/gmail/com/varlamvanadia1996/tests/SimpleTests3.java
+++ src/test/java/gmail/com/varlamvanadia1996/tests/SimpleTests3.java
@@ -3,15 +3,15 @@ package gmail.com.varlamvanadia1996.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SimpleTests {
+public class SimpleTests3 {
     @Test
     void SimpleTest1(){
-        Assertions.assertTrue(2>3);
+        Assertions.assertTrue(2<3);
     }
 
     @Test
     void SimpleTest2(){
-        Assertions.assertTrue(1>3);
+        Assertions.assertTrue(1<3);
     }
 
     @Test
@@ -21,7 +21,7 @@ public class SimpleTests {
 
     @Test
     void SimpleTest4(){
-        Assertions.assertTrue(1!=3);
+        Assertions.assertTrue(1!=4);
     }
 
 }
