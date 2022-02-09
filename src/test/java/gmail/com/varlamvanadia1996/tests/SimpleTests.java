76e94603604d1fbba884b947f90c511e3813c395
varlamova <varlamvanadia@mail.ru>
2022-02-09 19:20:50 +0300

    add test 2>3

---
+++ src/test/java/gmail/com/varlamvanadia1996/tests/SimpleTests.java
@@ -0,0 +1,11 @@
+package gmail.com.varlamvanadia1996.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests {
+    @Test
+    void SimpleTest1(){
+        Assertions.assertTrue(2>3);
+    }
+}
