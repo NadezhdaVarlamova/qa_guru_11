905acfc576b06bb4d6f81588de154dd6559bc9aa
NadezhdaVarlamova <96374770+NadezhdaVarlamova@users.noreply.github.com>
2022-02-10 18:29:12 +0300

    Add simpl tests (#1)
    добавлены тесты
    
    Co-authored-by: varlamova <varlamvanadia@mail.ru>

---
+++ src/test/java/gmail/com/varlamvanadia1996/tests/SimpleTests1.java
@@ -0,0 +1,28 @@
+package gmail.com.varlamvanadia1996.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests1 {
+    @Test
+    void SimpleTest1(){
+        Assertions.assertTrue(2<3);
+    }
+
+    @Test
+    void SimpleTest2(){
+        Assertions.assertTrue(1<3);
+    }
+
+    @Test
+    void SimpleTest3(){
+        Assertions.assertTrue(3==3);
+    }
+
+    @Test
+    void SimpleTest4(){
+        Assertions.assertTrue(1!=4);
+    }
+
+}
+
